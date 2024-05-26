a4a6186cc49475ee69ed017aee7555c178934969
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-05-26 18:22:41 +0300

    Add WeatherRecord model

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class WeatherRecord(models.Model):
+    date = models.DateField()
+    temperature = models.IntegerField()
+    description = models.CharField(max_length=100)
+    precipitation = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.date)
