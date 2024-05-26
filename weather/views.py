55716a9b648c1e3ec9729d1e97a3ba4fe1cc2615
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-05-26 19:56:24 +0300

    Create WeatherViewSet

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from weather.models import WeatherRecord
+from weather.serializers import WeatherSerializer
+
+
+class WeatherViewSet(
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = WeatherRecord.objects.all()
+    serializer_class = WeatherSerializer
