d52b6a01b7e776bc8dae781c88aa4ff58ff9014e
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-05-29 19:25:44 +0300

    Add celery with empty tasks.py

---
+++ weather/celery.py
@@ -0,0 +1,11 @@
+from celery import Celery
+
+
+app = Celery("weather")
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
