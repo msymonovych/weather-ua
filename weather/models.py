bbf36482cadf8891ad13d22fcf46b91ed3479444
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-06-10 12:09:05 +0300

    Change the data type of the fields for model

--- weather/models.py
+++ weather/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class WeatherRecord(models.Model):
-    date = models.DateField()
-    temperature = models.IntegerField()
+    date = models.DateField(unique=True)
+    temperature = models.FloatField()
     description = models.CharField(max_length=100)
-    precipitation = models.CharField(max_length=100)
+    precipitation = models.FloatField()
 
     def __str__(self):
-        return str(self.date)
+        return f"{self.date} - {self.temperature}°C"
