3f26b3321710978b0c40c6aa41a610092680151d
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-06-11 12:06:09 +0300

    Refactor save_weather_record

--- weather/tasks.py
+++ weather/tasks.py
@@ -4,31 +4,23 @@ from weather.utils import get_weather, WeatherRecordDataclass
 from weather.models import WeatherRecord
 
 
-def update_or_create_record(record: WeatherRecordDataclass):
-    if WeatherRecord.objects.filter(date=record.date).exists():
-        WeatherRecord.objects.filter(date=record.date).update(
-            date=record.date,
-            temperature=record.temperature,
-            description=record.description,
-            precipitation=record.precipitation,
-        )
-    else:
-        WeatherRecord.objects.create(
-            date=record.date,
-            temperature=record.temperature,
-            description=record.description,
-            precipitation=record.precipitation,
-        )
+def save_weather_record(record: WeatherRecordDataclass):
+    WeatherRecord.objects.update_or_create(
+        date=record.date,
+        temperature=record.temperature,
+        description=record.description,
+        precipitation=record.precipitation,
+    )
 
 
 @shared_task
 def get_today_weather():
     record = get_weather()
-    update_or_create_record(record[0])
+    save_weather_record(record[0])
 
 
 @shared_task
 def get_weather_for_five_days():
     records = get_weather(days=5)
     for record in records:
-        update_or_create_record(record)
+        save_weather_record(record)
